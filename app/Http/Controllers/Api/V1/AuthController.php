ceefbe597849e624114c7d849b6cbfb715ac3c62
Tai Wai David Chan <>
2024-07-15 16:07:42 +0800

    Added usage of voucher service create voucher for user in register function of auth controller.

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -5,11 +5,18 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Api\BaseApiController;
 use App\Http\Requests\UserRegisterRequest;
 use App\Models\User;
+use App\Services\VoucherService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class AuthController extends BaseApiController
 {
+    protected $voucherService;
+
+    public function __construct(VoucherService $voucherService)
+    {
+        $this->voucherService = $voucherService;
+    }
     /**
      * Register a new user.
      *
@@ -21,6 +28,8 @@ class AuthController extends BaseApiController
     public function register(UserRegisterRequest $request): JsonResponse
     {
         $user = User::create($request->validated());
+
+        $this->voucherService->createVoucherForUser($user->id);
 
         return $this->created($user, 'User registered successfully.', 201);
     }
