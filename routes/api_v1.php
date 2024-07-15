bfd6804540bb5736a7be417ec3c67d3797ca4d92
Tai Wai David Chan <>
2024-07-15 18:52:04 +0800

    Added endpoints for generating more vouchers codes, viewing voucher code, and deletion of voucher code for authenticated user.

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,13 +1,18 @@
 <?php
 use App\Http\Controllers\Api\v1\AuthController as AuthControllerV1;
+use App\Http\Controllers\Api\v1\VoucherController as VoucherControllerV1;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::post('/register', [AuthControllerV1::class, 'register']);
     Route::post('/login', [AuthControllerV1::class, 'login']);
-    Route::post('/logout', [AuthControllerV1::class, 'logout'])->middleware('auth:sanctum');
+    Route::post('/logout', [AuthControllerV1::class, 'logout'])->middleware('auth:api');
 
+    Route::middleware('auth:api')->group(function () {
+        Route::apiResource('/vouchers', VoucherControllerV1::class)->except(['create', 'edit']);
+    });
+    
     Route::middleware('auth:api')->get('/user', function (Request $request) {
         return $request->user();
     });
