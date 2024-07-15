65253f0cbd774a8890c9a0016219856d026104ba
Tai Wai David Chan <>
2024-07-15 18:05:18 +0800

    Implemented login and logout endpoints and added return of authenticated user.

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Api\BaseApiController;
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\UserRegisterRequest;
 use App\Jobs\SendWelcomeEmailJob;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -26,5 +28,46 @@ class AuthController extends BaseApiController
         SendWelcomeEmailJob::dispatch($user->id);
 
         return $this->created($user, 'User registered successfully.', 201);
+    }
+
+    /**
+     * Login a user.
+     *
+     *
+     * @param Request $request
+     *
+     * @return JsonResponse
+     */
+    public function login(LoginRequest $request): JsonResponse
+    {
+        $credentials = $request->only('username', 'password');
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return $this->success(['access_token' => $token, 'token_type' => 'Bearer'], 'Login successfully.');
+    }
+
+    /**
+     * Logout a user.
+     *
+     *
+     * @param Request $request
+     *
+     * @return JsonResponse
+     */
+    public function logout(Request $request): JsonResponse
+    {
+        $user = $request->user();
+        if ($user) {
+            $user->tokens()->delete();
+            return $this->success(null, 'Logged out successfully', 200);
+        }
+
+        return $this->error('Failed to log out', ['message' => 'No authenticated user found'], 401);
     }
 }
